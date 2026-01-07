f7d379ac6013d45c195ea65293122ee064bc1e06
Smriti Bhadouriya <smriti.marcadeo@gmail.com>
2026-01-07 16:39:17 +0530

    final fix

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Logo from '../assets/logo.png'
+import Logo from '../assets/onlylogo.png'; // Replace with the path to your image
 const Loading = () => {
   return (
     <div
@@ -7,15 +7,15 @@ const Loading = () => {
       role="status"
       aria-label="Loading"
     >
-      <div className="relative w-24 h-24 flex items-center justify-center">
+      <div className="relative w-20 h-20 flex items-center justify-center">
         {/* Circular Image (Placeholder, replace with actual image) */}
         <img
           src={Logo} // Replace with your image URL
           alt="Loading indicator"
-          className="w-16 h-16 rounded-full object-cover"
+          className="w-10 h-10  object-contain"
         />
         {/* Spinning Border */}
-        <div className="absolute w-full h-full border-4 border-t-transparent border-blue-500 rounded-full animate-spin"></div>
+        <div className="absolute w-full h-full border-4 border-t-transparent border-blue-900 rounded-full animate-spin"></div>
       </div>
     </div>
   );
